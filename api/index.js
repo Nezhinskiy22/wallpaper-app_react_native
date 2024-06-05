40654181f7e69194ab3c4a7238beb1179b4fb969
Nezhinskiy22 <Nezhinskiy22@gmail.com>
2024-06-05 20:52:28 +0300

    starting masonry list

---
+++ api/index.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+const API_KEY = "44239376-462a72ece4954f3d5a0368187";
+
+const apiUrl = `https://pixabay.com/api/?key=${API_KEY}`;
+
+const formatUrl = (params) => {
+  //{q, page, category, order}
+  let url = apiUrl + "&per_page=25&safesearch=true&editors_choise=true";
+  if (!params) return url;
+  let paramKeys = Object.keys(params);
+  paramKeys.map((key) => {
+    let value = key == "q" ? encodeURIComponent(params[key]) : params[key];
+    url += `&${key}=${value}`;
+  });
+  console.log("final url", url);
+  return url;
+};
+
+export const apiCall = async (params) => {
+  try {
+    const response = await axios.get(formatUrl(params));
+    const { data } = response;
+    return { success: true, data };
+  } catch (error) {
+    console.log("got error", error.message);
+    return { success: false, msg: error.message };
+  }
+};
+
+// const sample = {
+//     var API_KEY = "44239376-462a72ece4954f3d5a0368187";
+//     var URL =
+//       "https://pixabay.com/api/?key=" +
+//       API_KEY +
+//       "&q=" +
+//       encodeURIComponent("red roses");
+//     $.getJSON(URL, function (data) {
+//       if (parseInt(data.totalHits) > 0)
+//         $.each(data.hits, function (i, hit) {
+//           console.log(hit.pageURL);
+//         });
+//       else console.log("No hits");
+//     });
+//   }
--- app/index.js
+++ app/index.js
@@ -5,8 +5,11 @@ import { hp, wp } from "./../helpers/common";
 import { LinearGradient } from "expo-linear-gradient";
 import Animated, { FadeInDown } from "react-native-reanimated";
 import { theme } from "./../constants/theme";
+import { useRouter } from "expo-router";
 
 const App = () => {
+  const router = useRouter();
+
   return (
     <View style={styles.container}>
       <StatusBar style="light" />
@@ -43,7 +46,10 @@ const App = () => {
             Every Pixel Tells a Story
           </Animated.Text>
           <Animated.View entering={FadeInDown.delay(600).springify()}>
-            <Pressable style={styles.startButton}>
+            <Pressable
+              onPress={() => router.push("home")}
+              style={styles.startButton}
+            >
               <Text style={styles.startText}>Start Explore</Text>
             </Pressable>
           </Animated.View>
